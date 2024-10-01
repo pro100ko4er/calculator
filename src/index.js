c2e128339a07a44eb6b7290bc402de669098ff70
pro100ko4er <ai329390@gmail.com>
2024-10-01 19:48:22 +0300

    fixes to module

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import Calculator from './calculator.js';
 import Parser from './parser.js';
+import * as readline from 'node:readline/promises';
+import {
+    stdin as input,
+    stdout as output,
+} from 'node:process';
+
+
 
 const calculator = new Calculator(new Parser());
 
-const input = process.argv[2];
 
-if (!input) {
+const rl = readline.createInterface({ input, output });
+
+const expression = await rl.question('Введите выражение: ');
+
+if (!expression) {
   console.error('Пожалуйста, введите математическое выражение.');
   process.exit(1);
 }
 
 try {
-  const result = calculator.calculate(input);
+  const result = calculator.calculate(expression);
   console.log(`Результат: ${result}`);
 } catch (error) {
   console.error(`Ошибка: ${error.message}`);
   process.exit(1);
+}
+finally {
+  rl.close()
 }
